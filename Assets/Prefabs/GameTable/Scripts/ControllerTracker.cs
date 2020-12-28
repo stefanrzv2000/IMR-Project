f47497234e5472b1f9ab1c3d756d0394046f0607
Stefan Balauca <57128020+stefanrzv2000@users.noreply.github.com>
2020-12-28 21:44:17 +0200

    Interactions 2

---
+++ Assets/Prefabs/GameTable/Scripts/ControllerTracker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static VRHandController;
+
+public class ControllerTracker : MonoBehaviour
+{
+    public GameObject tracked = null;
+    private Material m0, m1;
+    int curr = -1;
+    float[] limx, limz;
+    private TableColors tableColors;
+
+    public HandType handType = HandType.RIGHT;
+    private string handName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handName = handType == HandType.RIGHT ? "RightHand" : "LeftHand";
+
+        tableColors = GetComponent<TableColors>();
+        //transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material = selected;
+
+        limx = new float[2];
+        limz = new float[2];
+
+        limx[0] = transform.position.x - 0.78f / 2;
+        limx[1] = transform.position.x + 0.78f / 2;
+        limz[0] = transform.position.z - 0.78f / 2;
+        limz[1] = transform.position.z + 0.78f / 2;
+        Debug.Log($"limx {limx[0]} {limx[1]}");
+        Debug.Log($"limz {limz[0]} {limz[1]}");
+    }
+
+    int GetSelectedChild()
+    {
+        float tx = tracked.transform.position.x;
+        float tz = tracked.transform.position.z;
+
+        if (limx[0] < tx && tx < limx[1] && limx[0] < tz && tz < limz[1])
+        {
+            int i = (int)(6 * (limx[1] - tx) / (limx[1] - limx[0]));
+            int j = (int)(6 * (limz[1] - tz) / (limz[1] - limz[0]));
+
+            return i * 6 + j;
+        }
+
+        return -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tracked == null)
+        {
+            tracked = GameObject.Find(handName);
+        }
+        if (tracked != null)
+        {
+            tableColors.SetHover(GetSelectedChild());
+        }
+    }
+}
