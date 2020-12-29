a8efca756033c3ed5f2775c6ff74224cc38892ce
Stefan Balauca <57128020+stefanrzv2000@users.noreply.github.com>
2020-12-29 22:59:01 +0200

    Menu Updates

--- Assets/Scripts/PhotonScripts/ButtonScript.cs
+++ Assets/Scripts/PhotonScripts/ButtonScript.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine;
 using VRTK;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public UnityEvent OnClickMenuButton;
+    private Color lastColor = Color.black;
 
     void Start()
     {
@@ -24,14 +26,28 @@ public class ButtonScript : MonoBehaviour
     protected virtual void OnEnable()
     {
         linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+        if (lastColor == Color.black) lastColor = transform.GetChild(0).Find("Text").GetComponent<Text>().color;
 
         if (linkedObject != null)
         {
             linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            linkedObject.InteractableObjectTouched += InteractableObjectTouched;
+            linkedObject.InteractableObjectUntouched += InteractableObjectUntouched;
             //linkedObject.InteractableObjectUnused += InteractableObjectUnused;
         }
 
         //spinner = transform.Find("Capsule");
+    }
+
+    private void InteractableObjectUntouched(object sender, InteractableObjectEventArgs e)
+    {
+        transform.GetChild(0).Find("Text").GetComponent<Text>().color = lastColor;
+    }
+
+    private void InteractableObjectTouched(object sender, InteractableObjectEventArgs e)
+    {
+        if (lastColor == Color.black) lastColor = transform.GetChild(0).Find("Text").GetComponent<Text>().color;
+        transform.GetChild(0).Find("Text").GetComponent<Text>().color = Color.red;
     }
 
     private void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
@@ -41,6 +57,7 @@ public class ButtonScript : MonoBehaviour
 
     protected virtual void OnDisable()
     {
+        transform.GetChild(0).Find("Text").GetComponent<Text>().color = lastColor;
         if (linkedObject != null)
         {
             linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
